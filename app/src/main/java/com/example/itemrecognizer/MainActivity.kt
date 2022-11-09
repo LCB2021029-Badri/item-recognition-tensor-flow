f176798a6f1288072f3d35c2024c30e92ae5ebfb
badri_akkala <lcb2021029@iiitl.ac.in>
2022-11-10 02:53:26 +0530

    step 5 : tflite model execution started, but still didnt extract output properly

--- app/src/main/java/com/example/itemrecognizer/MainActivity.kt
+++ app/src/main/java/com/example/itemrecognizer/MainActivity.kt
@@ -8,6 +8,10 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import com.example.itemrecognizer.databinding.ActivityMainBinding
+import com.example.itemrecognizer.ml.MobilenetV110224Quant
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,17 +25,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //setting the image button to intent
-        binding.btnSelect.setOnClickListener (View.OnClickListener{
-            var intent:Intent = Intent(Intent.ACTION_GET_CONTENT)
+        binding.btnSelect.setOnClickListener(View.OnClickListener {
+            var intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "image/*"
-            startActivityForResult(intent,69)
+            startActivityForResult(intent, 69)
         })
 
         //coding functionality for predict
-        binding.btnPredict.setOnClickListener(View.OnClickListener{
+        binding.btnPredict.setOnClickListener(View.OnClickListener {
             //resizing the bitmap to the tflite data set image dimensions
-            var resized: Bitmap = Bitmap.createScaledBitmap(bitmap,224,224,true)
+            var resized: Bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true)
+
+            //copy-paste the code set in ".tflite" file here------------------copy pasted-----------
+            val model = MobilenetV110224Quant.newInstance(this)
+            // Creates inputs for reference.
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
+            //create byte buffer image form the acquired bitmap-------manual added 1----------------
+            var tbuffer = TensorImage.fromBitmap(resized)
+            var byteBuffer = tbuffer.buffer
+            //--------------------------------------------------------manual added 1----------------
+            inputFeature0.loadBuffer(byteBuffer)
+            // Runs model inference and gets result.
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer// use this result
+            //setting output image----------------------------manual added 2------------------------
+            binding.textView1.setText(outputFeature0.floatArray[96].toString()) // random index for dummy value
+            //------------------------------------------------manual added 2------------------------
+            // Releases model resources if no longer used.
+            model.close()
+            //---------------------------------------------------------------copy pasted------------
+
         })
+
+    }
 
     //once the user selects the image change the imageView1 to selected image and to store the image (in bitmap format)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
