a8360de237336e6b94d5dc4f9cebf4db6bfca842
badri_akkala <lcb2021029@iiitl.ac.in>
2022-11-10 02:34:19 +0530

    step 3 : Acquiring the image and setting the image to bitmap variable to pass it for model

--- app/src/main/java/com/example/itemrecognizer/MainActivity.kt
+++ app/src/main/java/com/example/itemrecognizer/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.itemrecognizer
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import com.example.itemrecognizer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    //for converting the image the user selects to bitmap
+    lateinit var bitmap: Bitmap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setting the image button to intent
+        binding.btnSelect.setOnClickListener {
+            var intent:Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent,69)
+        }
+
     }
+
+    //once the user selects the image change the imageView1 to selected image and to store the image (in bitmap format)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //setting the imageview
+        binding.imageView1.setImageURI(data?.data)
+        //storing the acquired image to bitmap to use it for predection
+        var uri : Uri? = data?.data
+        bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver,uri)
+    }
+
 }
